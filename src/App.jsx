1dde0a6b296eb0c5686e265578141bcdb7004d60
Ankita <ankitaag94@gmail.com>
2024-07-23 15:07:12 +0530

    set up Mantine UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
+import "@mantine/core/styles.css";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({});
 
 function App() {
-  return <></>;
+  return <MantineProvider theme={theme}>{}</MantineProvider>;
 }
 
 export default App;
