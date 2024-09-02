d4f1cf0435c539fa1bd3e7a2ff892ed0dabd442a
Ankita <ankitaag94@gmail.com>
2024-09-02 16:18:59 +0530

    set up mantine modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
 import "@mantine/core/styles.css";
-import { createTheme, Flex, Grid, MantineProvider, rem } from "@mantine/core";
+import { createTheme, MantineProvider, rem } from "@mantine/core";
 import AddNewBook from "./components/AddNewBook/AddNewBook";
 import NavBar from "./components/NavBar/NavBar";
 import useBooks from "./hooks/useBooks";
 import { ToastContainer } from "react-toastify";
 import RenderBook from "./components/RenderBook/RenderBook";
+import { ModalsProvider } from "@mantine/modals";
 
 const theme = createTheme({
   white: "#F5F5F5",
   black: "#5b3e3c",
-  fontFamily: "sans-serif",
+  fontFamily: "Poppins, sans-serif",
   fontSizes: {
     xs: rem(12),
     sm: rem(14),
@@ -22,12 +23,12 @@ const theme = createTheme({
   lineHeights: {
     xs: "1.3",
     sm: "1.45",
-    md: "1.55", //md default value
+    md: "1.55",
     lg: "1.65",
     xl: "1.85",
   },
   headings: {
-    //properties for all headings
+    fontFamily: "Inter, sans-serif",
     fontWeight: "400",
     textWrap: "pretty",
     sizes: {
@@ -41,9 +42,13 @@ const theme = createTheme({
         fontSize: rem(53),
         lineHeight: "1.2",
       },
+      h3: {
+        fontWeight: "600",
+        fontSize: rem(20),
+        lineHeight: "1.2",
+      },
     },
   },
-  // colors: open color
   primaryColor: "orange",
   primaryShade: 6,
   breakpoints: {
@@ -59,16 +64,18 @@ function App() {
   const { books, addNewBook } = useBooks();
   return (
     <MantineProvider theme={theme}>
-      <NavBar />
-      <AddNewBook addNewBook={addNewBook} />
-      <div className="grid-container">
-        {books.map((book) => (
-          <div span={2} className="" key={book.id}>
-            <RenderBook book={book} />
-          </div>
-        ))}
-      </div>
-      <ToastContainer />
+      <ModalsProvider>
+        <NavBar />
+        <AddNewBook addNewBook={addNewBook} />
+        <div className="grid-container">
+          {books.map((book) => (
+            <div span={2} className="" key={book.id}>
+              <RenderBook book={book} />
+            </div>
+          ))}
+        </div>
+        <ToastContainer />
+      </ModalsProvider>
     </MantineProvider>
   );
 }
