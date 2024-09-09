20b4281170fd0c4f203f0de04a05eb97744b5081
Ankita <ankitaag94@gmail.com>
2024-09-09 18:29:13 +0530

    improve layout

--- src/components/RenderAllBooks/RenderAllBooks.jsx
+++ src/components/RenderAllBooks/RenderAllBooks.jsx
@@ -1,9 +1,15 @@
 import { Flex } from "@mantine/core";
 import RenderBook from "../RenderBook/RenderBook";
+import classes from "./styles.module.css";
 
 function RenderAllBooks({ books }) {
   return (
-    <Flex wrap={"wrap"} justify={"space-between"} gap={"2rem"}>
+    <Flex
+      wrap={"wrap"}
+      justify={"space-between"}
+      gap={"2rem"}
+      className={classes.allBooks}
+    >
       {books.map((book) => (
         <div key={book.id}>
           <RenderBook book={book} />
