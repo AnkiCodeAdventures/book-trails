11dc05babf501057b1b5ba6911fdeb67079dd6a9
Ankita <ankitaag94@gmail.com>
2024-10-05 13:29:29 +0530

    prepare for deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const API_ENDPOINT = "http://localhost:3000/";
+export const API_ENDPOINT =
+  import.meta.env.VITE_API_ENDPOINT || "http://localhost:3000/";
 
 export const API_ROUTES = {
   BOOKS: `${API_ENDPOINT}books`,
